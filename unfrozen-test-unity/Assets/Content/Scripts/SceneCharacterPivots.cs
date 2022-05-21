632dacffc9915c777689f80dc8b61c82e7b99962
makovshe <makovshe@gmail.com>
2022-05-21 15:21:55 +0300

    Add: - artForTestWork content - characters prefabs - battle scene - scripts for test work (part of work)

---
+++ unfrozen-test-unity/Assets/Content/Scripts/SceneCharacterPivots.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Content.Scripts
+{
+    public sealed class SceneCharacterPivots : MonoBehaviour
+    {
+        public Transform[] LeftTeam;
+        public Transform[] RightTeam;
+    }
+}
